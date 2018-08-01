03bd8c04052c01a836b6d0eb58f748fdae1f81df
marktwtn <marktwtn@gmail.com>
2018-08-02 02:18:31 +0800

    add the structure and functions of the singly-linked list

---
+++ list.c
@@ -0,0 +1,43 @@
+node_t *node_delete(int val) {
+    node_t *prev, *current;
+    prev = &ListHead;
+
+    pthread_mutex_lock(&prev->lock);
+    while ((current = prev->link)) {
+        pthread_mutex_lock(&current->lock);
+        if (current->val == val) {
+            prev->link = current->link;
+            current->link = NULL;
+            pthread_mutex_unlock(&prev->lock);
+            pthread_mutex_unlock(&current->lock);
+            return current;
+        }
+        pthread_mutex_unlock(&prev->lock);
+        prev = current;
+    }
+    pthread_mutex_unlock(&prev->lock);
+    return NULL;
+}
+
+int node_insert(int val, int target) {
+    node_t *prev, *current;
+    prev = &ListHead;
+
+    pthread_mutex_lock(&prev->lock);
+    while ((current = prev->link)) {
+        pthread_mutex_lock(&current->lock);
+        if (current->val == target) {
+            node_t *newNode;
+            newNode->val = val;
+            newNode->link = current->link;
+            current->link = newNode;
+            pthread_mutex_unlock(&current->lock);
+            pthread_mutex_unlock(&prev->lock);
+            return 0;
+        }
+        pthread_mutex_unlock(&prev->lock);
+        prev = current;
+    }
+    pthread_mutex_unlock(&prev->lock);
+    return (-1);
+}
---
+++ list.h
@@ -0,0 +1,12 @@
+#include <pthread.h>
+
+typedef struct node {
+    int val;
+    struct node *link;
+    pthread_mutex_t lock;
+} node_t;
+
+node_t ListHead = { .val = 0 };
+
+node_t *node_delete(int val);
+int node_insert(int val, int target);
