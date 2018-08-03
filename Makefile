8db8e508d728a85eb23147d81d0638bc16db8802
marktwtn <marktwtn@gmail.com>
2018-08-03 10:30:04 +0800

    add the code of multi-thread

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: all
 all: main.c list.c list.h
-	gcc main.c list.c
+	gcc main.c list.c -lpthread
 
 run: all
 	./a.out
--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include "list.h"
 
+#define THREAD_NUM 2
+
+void *child(void *arg) {
+    pthread_exit(NULL);
+}
+
 int main(void) {
+    pthread_t thr[THREAD_NUM];
+
+    for(int i=0; i<THREAD_NUM; i++) {
+        pthread_create(&thr[i], NULL, child, NULL);
+    }
+
+    for(int i=0; i<THREAD_NUM; i++) {
+        pthread_join(thr[i], NULL);
+    }
+
     return 0;
 }
